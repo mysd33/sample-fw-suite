d7d64967b46bf4b6aa8f68de86cf6d0787acbae3
mysd33 <mysd.33@gmail.com>
2026-04-16 23:07:32 +0900

    ログのメッセージID修正

--- sample-fw/sample-fw-batch/src/main/java/com/example/fw/batch/jobflow/sfn/SfnTaskResultSenderStub.java
+++ sample-fw/sample-fw-batch/src/main/java/com/example/fw/batch/jobflow/sfn/SfnTaskResultSenderStub.java
@@ -43,7 +43,7 @@ public class SfnTaskResultSenderStub implements SfnTaskResultSender {
 
     @Override
     public void resendTaskSuccessByJsonString(String taskToken, String outputJson) {
-        appLogger.info(BatchFrameworkMessageIds.I_FW_JBFLW_0001, taskToken, outputJson);
+        appLogger.info(BatchFrameworkMessageIds.I_FW_JBFLW_0002, taskToken, outputJson);
         // 再送信なのでDBの永続化は行わない
         // StepFunctionsには送信せず、ログ出力のみで完了とする
     }
